c42007c3a6b699714f926585c047825b0468749e
Jeremy Peters <info.peters@quantentunnel.de>
2019-04-14 20:28:15 +0200

    Added about box

--- PC_Verwaltung/Manager.xaml.cs
+++ PC_Verwaltung/Manager.xaml.cs
@@ -12,15 +12,31 @@ namespace PC_Verwaltung
         {
             InitializeComponent();
 
+            // for debugging
+            User.CurrentUser = Register.Users[0];
+
+            /*
             // Anmeldung aufrufen
             if (new Entry().ShowDialog() == false)
             {
                 // Anwendung schließen, wenn niemand angemeldet wurde
                 Close();
             }
+            */
         }
 
-        // Event handler zum schließen der Anwendung
+        // Event handler zum Anzeigen des Infofensters
+        private void AboutClickEvent(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Dies ist eine einfache PC Verwaltung\n"
+                + "ohne wirklichen Nutzen, außer ein\n"
+                + "hoffentlich passables Schulprojekt zu sein.\n\n"
+                + "Erstellt mit Liebe von Jeremy Peters.", 
+                FindResource("AppTitle").ToString());
+        }
+
+        // Event handler zum Schließen der Anwendung
         private void CloseClickEvent(object sender, System.Windows.RoutedEventArgs e)
         {
             Close();
