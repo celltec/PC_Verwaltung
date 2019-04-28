da90d04f7a64b83c0aeb3d06f6578464f2d81a5b
Jeremy Peters <info.peters@quantentunnel.de>
2019-04-28 12:02:38 +0200

    Save button saves only to current file

--- PC_Verwaltung/Manager.xaml.cs
+++ PC_Verwaltung/Manager.xaml.cs
@@ -23,12 +23,16 @@ namespace PC_Verwaltung
         {
             InitializeComponent();
 
+            /*
             // Anmeldung aufrufen
             if (new Entry().ShowDialog() == false)
             {
                 // Anwendung schließen, wenn niemand angemeldet wurde
                 Close();
             }
+            */
+
+            User.CurrentUser = Register.Users[0]; // for debugging
 
             // TODO: change implementation of user assignment
             Xml = new XmlSerializer(typeof(List<Computer>), new XmlRootAttribute(User.CurrentUser.UserName));
@@ -87,45 +91,51 @@ namespace PC_Verwaltung
             }
         }
 
-        // Schreibt die derzeitige Konfiguration in eine .xml Datei
+        // Schreibt die derzeitige Konfiguration in die geöffnete .xml Datei
         private void SaveConfiguration()
+        {
+            // Liste mit allen Computern, die dem derzeitigen Benutzer zugewiesen sind
+            List<Computer> Computers = new List<Computer>();
+
+            foreach (ComputerView view in ComputerViewList.Children)
+            {
+                // TODO: check for empty or illegal characters
+                Computers.Add(new Computer(int.Parse(view.TextBoxId.Text), view.TextBoxName.Text, view.TextBoxMac.Text));
+            }
+
+            // Erstelle Schreibeobjekt
+            FileStream Writer = new FileStream(XmlFileDialog.FileName, FileMode.Create, FileAccess.Write);
+
+            // Für bessere Ästhetik unnötige Angaben über den Namespace unterdrücken 
+            XmlSerializerNamespaces EmptyNameSpace = new XmlSerializerNamespaces();
+            EmptyNameSpace.Add(string.Empty, string.Empty);
+
+            try
+            {
+                // Konvertiere die Daten der Computer Objekte in das XML Format
+                Xml.Serialize(Writer, Computers, EmptyNameSpace);
+            }
+            catch (Exception e)
+            {
+                // Zeige eine Fehlermeldung wenn etwas schief gegangen ist
+                MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                // Schreibeobjekt muss geschlossen werden
+                Writer.Close();
+            }
+        }
+
+        // Ruft die Methode zum Speichern für eine neue .xml Datei auf
+        private void SaveAsConfiguration()
         {
             XmlFileDialog = new SaveFileDialog{ Filter = "XML Dateien (*.xml)|*.xml" };
 
             // Fragen wo die Datei gespeichert werden soll
             if (XmlFileDialog.ShowDialog() == true)
             {
-                // Liste mit allen Computern, die dem derzeitigen Benutzer zugewiesen sind
-                List<Computer> Computers = new List<Computer>();
-
-                foreach (ComputerView view in ComputerViewList.Children)
-                {
-                    // TODO: check for empty or illegal characters
-                    Computers.Add(new Computer(int.Parse(view.TextBoxId.Text), view.TextBoxName.Text, view.TextBoxMac.Text));
-                }
-
-                // Erstelle Schreibeobjekt
-                FileStream Writer = new FileStream(XmlFileDialog.FileName, FileMode.Create, FileAccess.Write);
-
-                // Für bessere Ästhetik unnötige Angaben über den Namespace unterdrücken 
-                XmlSerializerNamespaces EmptyNameSpace = new XmlSerializerNamespaces();
-                EmptyNameSpace.Add(string.Empty, string.Empty);
-
-                try
-                {
-                    // Konvertiere die Daten der Computer Objekte in das XML Format
-                    Xml.Serialize(Writer, Computers, EmptyNameSpace);
-                }
-                catch (Exception e)
-                {
-                    // Zeige eine Fehlermeldung wenn etwas schief gegangen ist
-                    MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-                finally
-                {
-                    // Schreibeobjekt muss geschlossen werden
-                    Writer.Close();
-                }
+                SaveConfiguration();
             }
         }
 
@@ -155,17 +165,23 @@ namespace PC_Verwaltung
             OpenConfiguration();
         }
 
-        // Event handler zum Speichern der Konfiguration in der aktuellen Datei
+        // Event handler zum Speichern der Konfiguration
         private void SaveClickEvent(object sender, System.Windows.RoutedEventArgs e)
         {
-            // TODO: save only to current file
-            SaveConfiguration();
+            if (XmlFileDialog == null || XmlFileDialog.FileName == "")
+            {
+                SaveAsConfiguration();
+            }
+            else
+            {
+                SaveConfiguration();
+            }
         }
 
         // Event handler zum Speichern der Konfiguration in einer neuen Datei
         private void SaveAsClickEvent(object sender, System.Windows.RoutedEventArgs e)
         {
-            SaveConfiguration();
+            SaveAsConfiguration();
         }
 
         // Event handler zum Schließen der Anwendung
